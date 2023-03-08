bf4eea89a6326939917931e766ec1acffbb462c3
Connor Carciofini <ccarciofini@gmail.com>
2023-03-07 19:09:21 -0600

    Initial Commit

---
+++ calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by Connor on 3/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Connor on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
